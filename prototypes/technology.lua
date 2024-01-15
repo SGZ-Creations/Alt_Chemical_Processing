0b1763c3a6c4feeb753f4dca8ae91a8103020456
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2024-01-15 11:52:25 +0100

    0.0.3 Fixes.

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,9 +1,17 @@
 local fueltech = table.deepcopy(data.raw.technology["rocket-fuel"])
-fueltech.name = "rocket-fuel-bob_alt"
-fueltech.effects = {{type = "unlock-recipe", recipe = "rocket-fuel-from-solid-fuel"},{type = "unlock-recipe", recipe = "rocket-fuel-from-enriched-fuel"}}
-fueltech.prerequisites = {"rocket-fuel", "hydrazine", "solid-fuel"}
-fueltech.unit = 
+fueltech.name = "rocket-fuel-2"
+fueltech.effects = {
     {
+        type = "unlock-recipe",
+        recipe = "rocket-fuel-from-solid-fuel"
+    },
+    {
+        type = "unlock-recipe",
+        recipe = "rocket-fuel-from-enriched-fuel"
+    }
+}
+fueltech.prerequisites = {"rocket-fuel",}
+    fueltech.unit = {
         count = 300,
         ingredients =
         {
