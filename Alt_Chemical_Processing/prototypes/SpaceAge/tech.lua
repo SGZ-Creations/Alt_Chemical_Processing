dc04525ea2110f207ba035a9f2f463796cc11269
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-03-09 06:49:25 +0100

    Crash with missing tech.

--- Alt_Chemical_Processing/prototypes/SpaceAge/tech.lua
+++ Alt_Chemical_Processing/prototypes/SpaceAge/tech.lua
@@ -9,9 +9,13 @@ if mods["space-age"] then
 	if SS["seed-spoilage"].value then
 		table.insert(DRT["biochamber"].effects, {type="unlock-recipe", recipe="spoiled-seed"})
 	end
-
+end
+if mods["More-Asteroid-Resources-Forked"] and mods["space-age"] then
 	if SS["carbon-steel"].value then
 		table.insert(DRT["trace-carbonic-processing"].effects, {type="unlock-recipe", recipe="foundry-carbon-steel"})
 		table.insert(DRT["trace-carbonic-processing"].effects, {type="unlock-recipe", recipe="foundry-carbon-steel-plate"})
+	elseif mods["space-age"] and not mods["More-Asteroid-Resources-Forked"] then
+		table.insert(DRT["advanced-asteroid-processing"].effects, {type="unlock-recipe", recipe="foundry-carbon-steel"})
+		table.insert(DRT["advanced-asteroid-processing"].effects, {type="unlock-recipe", recipe="foundry-carbon-steel-plate"})
 	end
 end
