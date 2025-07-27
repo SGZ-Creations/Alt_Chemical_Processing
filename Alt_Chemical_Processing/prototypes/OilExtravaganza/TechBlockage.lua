fab3fb572d6424d463d91ab92f734da9a387f4fd
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-07-27 12:44:02 +0200

    Tweaks + mod required for change to take effect.

--- Alt_Chemical_Processing/prototypes/OilExtravaganza/TechBlockage.lua
+++ Alt_Chemical_Processing/prototypes/OilExtravaganza/TechBlockage.lua
@@ -1,5 +1,6 @@
 ---@class data.TechnologyPrototype
 local Tech = data.raw["technology"]
 
-
-Tech["oil-processing"].research_trigger = {type ="craft-item", item ="pumpjack", amount = 1}
+if mods["James-Oil-Processing"] then
+	Tech["oil-processing"].research_trigger = {type ="craft-item", item ="pumpjack", amount = 5}
+end
