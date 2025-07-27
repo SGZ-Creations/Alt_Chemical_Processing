02bac89b625c99a4294d4ddfa2cba999d247344f
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-07-27 14:35:34 +0200

    Less annoying lamps. Changelog

--- Alt_Chemical_Processing/prototypes/Base/Misc.lua
+++ Alt_Chemical_Processing/prototypes/Base/Misc.lua
@@ -1,4 +1,12 @@
 local SS = settings.startup
+---@class data.LampPrototype
+local Lamp = data.raw["lamp"]
 
-data.raw.projectile["cliff-explosives"].action[1].action_delivery.target_effects[3].radius = SS["ExplosiveRadius"].value
-data.raw.capsule["cliff-explosives"].capsule_action.attack_parameters.range = SS["ExplosiveRange"].value
+if mods["base"] then
+	data.raw.projectile["cliff-explosives"].action[1].action_delivery.target_effects[3].radius = SS["ExplosiveRadius"].value
+	data.raw.capsule["cliff-explosives"].capsule_action.attack_parameters.range = SS["ExplosiveRange"].value
+end
+
+if mods["base"] then
+	Lamp["small-lamp"].light = {intensity = 1, size = 50, color = {1, 1, 1}}
+end
