4f95b12bbcb5c8007139ce04e77aea59f6485c02
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-23 07:28:24 +0200

    few bug fixes

--- Alt_Chemical_Processing/prototypes/BetterRobotsExtended/RecipeChanges.lua
+++ Alt_Chemical_Processing/prototypes/BetterRobotsExtended/RecipeChanges.lua
@@ -5,47 +5,47 @@ if mods["Better_Robots_Extended"] and mods["bobelectronics"] and mods["bobplates
 	Recipe["BRE-logistic-robotics-mk2"].ingredients = {
 		{type = "item", name = "logistic-robot", amount = 1},
 		{type = "item", name = "electronic-circuit", amount = 5},
-		{type = "item", name = "bob-flying-frame-2", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-2", amount = 1},
 	}
 	Recipe["BRE-logistic-robotics-mk3"].ingredients = {
 		{type = "item", name = "BRE-logistic-robotics-mk2", amount = 1},
 		{type = "item", name = "advanced-circuit", amount = 5},
-		{type = "item", name = "bob-flying-frame-3", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-3", amount = 1},
 		{type = "item", name = "low-density-structure", amount = 1},
 	}
 	Recipe["BRE-logistic-robotics-mk4"].ingredients = {
 		{type = "item", name = "BRE-logistic-robotics-mk3", amount = 1},
 		{type = "item", name = "processing-unit", amount = 5},
-		{type = "item", name = "bob-flying-frame-4", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-4", amount = 1},
 		{type = "item", name = "bob-heat-shield-tile", amount = 2},
 	}
 	Recipe["BRE-logistic-robotics-mk5"].ingredients = {
 		{type = "item", name = "BRE-logistic-robotics-mk4", amount = 1},
-		{type = "item", name = "advanced-processing-unit", amount = 5},
-		{type = "item", name = "bob-flying-frame-4", amount = 1},
+		{type = "item", name = "bob-advanced-processing-unit", amount = 5},
+		{type = "item", name = "bob-flying-robot-frame-4", amount = 1},
 		{type = "item", name = "bob-rtg", amount = 1},
 	}
 	Recipe["BRE-construction-robotics-mk2"].ingredients = {
 		{type = "item", name = "construction-robot", amount = 1},
 		{type = "item", name = "electronic-circuit", amount = 5},
-		{type = "item", name = "bob-flying-frame-2", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-2", amount = 1},
 	}
 	Recipe["BRE-construction-robotics-mk3"].ingredients = {
 		{type = "item", name = "BRE-construction-robotics-mk2", amount = 1},
 		{type = "item", name = "advanced-circuit", amount = 5},
-		{type = "item", name = "bob-flying-frame-3", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-3", amount = 1},
 		{type = "item", name = "low-density-structure", amount = 1},
 	}
 	Recipe["BRE-construction-robotics-mk4"].ingredients = {
 		{type = "item", name = "BRE-construction-robotics-mk3", amount = 1},
 		{type = "item", name = "processing-unit", amount = 5},
-		{type = "item", name = "bob-flying-frame-4", amount = 1},
+		{type = "item", name = "bob-flying-robot-frame-4", amount = 1},
 		{type = "item", name = "bob-heat-shield-tile", amount = 2},
 	}
 	Recipe["BRE-construction-robotics-mk5"].ingredients = {
 		{type = "item", name = "BRE-construction-robotics-mk4", amount = 1},
-		{type = "item", name = "advanced-processing-unit", amount = 5},
-		{type = "item", name = "bob-flying-frame-4", amount = 1},
+		{type = "item", name = "bob-advanced-processing-unit", amount = 5},
+		{type = "item", name = "bob-flying-robot-frame-4", amount = 1},
 		{type = "item", name = "bob-rtg", amount = 1},
 	}
 end
