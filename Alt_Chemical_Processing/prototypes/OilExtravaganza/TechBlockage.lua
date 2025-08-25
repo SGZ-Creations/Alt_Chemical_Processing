2a839cc3cc0174ba0dd3091be422ded2a92a8636
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-25 09:57:48 +0200

    Misssing Tech

--- Alt_Chemical_Processing/prototypes/OilExtravaganza/TechBlockage.lua
+++ Alt_Chemical_Processing/prototypes/OilExtravaganza/TechBlockage.lua
@@ -3,4 +3,8 @@ local Tech = data.raw["technology"]
 
 if mods["James-Oil-Processing"] then
 	Tech["oil-processing"].research_trigger = {type ="craft-item", item ="pumpjack", amount = 5}
+
+	if mods["space-age"]then
+		Tech["basic-syngas-processing"].effects = {type ="unlock-recipe", recipe ="ChemicalSyngas"}
+	end
 end
