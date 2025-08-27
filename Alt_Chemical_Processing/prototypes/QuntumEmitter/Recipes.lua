4c98df2696e9d92b74297a29363c147fa4f77963
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-08-27 15:50:42 +0200

    QuantumDiffuser

---
+++ Alt_Chemical_Processing/prototypes/QuntumEmitter/Recipes.lua
@@ -0,0 +1,18 @@
+
+
+if mods["Quantum-diffuser"]then
+	Recipe["quantum-emitter"].ingredients = {
+			{type = "item", name = "steel-plate", amount = 500},
+			{type = "item", name = "processing-unit", amount = 50},
+			{type = "item", name = "beacon", amount = 10}
+		}
+	if mods["space-age"]then
+		Recipe["quantum-emitter"].ingredients = {
+			{type = "item", name = "superconductor", amount = 200},
+			{type = "item", name = "tungsten-carbide", amount = 200},
+			{type = "item", name = "carbon-fiber", amount =200},
+			{type = "item", name = "processing-unit", amount = 50},
+			{type = "item", name = "beacon", amount = 2}
+		}
+	end
+end
---
+++ Alt_Chemical_Processing/prototypes/QuntumEmitter/Tech.lua
@@ -0,0 +1,31 @@
+---@class data.TechnologyPrototype
+local Tech = data.raw["technology"]
+
+
+if mods["Quantum-diffuser"]then
+	Tech["Quantum-Tech"].prerequisites = nil
+	Tech["Quantum-Tech"].hidden = true
+	Tech["quantum-emitter"].prerequisites = {"effect-transmission"}
+	Tech["quantum-emitter"].unit.ingredients = {
+		{"automation-science-pack",      1},
+		{"logistic-science-pack",        1},
+		{"chemical-science-pack",        1},
+		{"production-science-pack",      1},
+	}
+	if mods["space-age"] then
+		Tech["quantum-emitter"].unit.ingredients = {
+			{"automation-science-pack",      1},
+			{"logistic-science-pack",        1},
+			{"chemical-science-pack",        1},
+			{"production-science-pack",      1},
+			{"utility-science-pack",         1},
+			{"space-science-pack",           1},
+			{"metallurgic-science-pack",     1},
+			{"electromagnetic-science-pack", 1}
+		}
+	end
+	if mods["god-module"]then
+		table.insert(Tech["quantum-emitter"].prerequisites, "god-module")
+	end
+end
+
