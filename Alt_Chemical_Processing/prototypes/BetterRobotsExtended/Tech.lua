b6857ddbf3910b0b5ae9ef0397d6fa7aea68fa10
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-09-16 14:36:55 +0200

    last changes

--- Alt_Chemical_Processing/prototypes/BetterRobotsExtended/Tech.lua
+++ Alt_Chemical_Processing/prototypes/BetterRobotsExtended/Tech.lua
@@ -7,15 +7,15 @@ if mods["Better_Robots_Extended"] and mods["boblogistics"] then
 	--if SS["JunkParts"].value then
 		Tech["bob-robots-1"].prerequisites = nil
 		Tech["bob-robots-1"].dependencies = nil
-		--Tech["bob-robots-1"].hidden = true
+		Tech["bob-robots-1"].hidden = true
 
 		Tech["bob-robots-2"].prerequisites = nil
 		Tech["bob-robots-2"].dependencies = nil
-		--Tech["bob-robots-2"].hidden = true
+		Tech["bob-robots-2"].hidden = true
 
 		Tech["bob-robots-3"].prerequisites = nil
 		Tech["bob-robots-3"].dependencies = nil
-		--Tech["bob-robots-3"].hidden = true
+		Tech["bob-robots-3"].hidden = true
 
 		Tech["bob-robots-4"].prerequisites = nil
 		Tech["bob-robots-4"].dependencies = nil
