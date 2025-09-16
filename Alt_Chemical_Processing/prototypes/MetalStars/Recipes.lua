da82fd28fcbeb27ff387f742cb4eedd596b937a9
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-09-16 15:21:27 +0200

    Comming liquid & molten recipes.

---
+++ Alt_Chemical_Processing/prototypes/MetalStars/Recipes.lua
@@ -0,0 +1,10 @@
+---@class data.RecipePrototype
+local Recipe = data.raw.recipe
+
+if mods["metal-and-stars"] then
+table.insert(Recipe[""].resuilt)
+
+	if SS["WarmNix"].value then
+		data.raw.planet["Nix"].entities_require_heating = false
+	end
+end
