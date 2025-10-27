f272a1b28aaf2bd053ec1df674e4121bd160e27b
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-10-27 15:33:33 +0100

    Nuclear merging

--- Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
+++ Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
@@ -30,7 +30,6 @@ if mods["bobplates"] and mods["space-age"] then
     end
 end
 
-
 if mods["bobplates"] and mods["SaltedFish"] then
     local replacements = {
         ["bob-salt"] = "salt",
@@ -56,11 +55,13 @@ if mods["bobplates"] and mods["SaltedFish"] then
         end
     end
 end
+
 if mods["bobplates"]and mods["bobelectronics"] and mods["metal-and-stars"] then
     local replacements = {
         ["glass-plate"] = "bob-glass",
         ["gold-plate"] = "bob-gold-plate",
         ["gold-cable"] = "bob-gilded-copper-cable",
+        ["thorium"] = "bob-thorium-232"
     }
     for _, recipe in pairs(data.raw.recipe) do
         for _, ingredient in pairs(recipe.ingredients or {}) do
@@ -83,6 +84,33 @@ if mods["bobplates"]and mods["bobelectronics"] and mods["metal-and-stars"] then
         end
     end
 end
+
+if mods["Clowns-Nuclear"]and mods["metal-and-stars"] then
+    local replacements = {
+        ["uranuim-233"] = "uranium-235"
+    }
+    for _, recipe in pairs(data.raw.recipe) do
+        for _, ingredient in pairs(recipe.ingredients or {}) do
+            local replace = replacements[ingredient.name]
+				if replace then
+                ingredient.name = replace
+            end
+        end
+
+		for _, result in pairs(recipe.results or {}) do
+			local replace = replacements[result.name]
+				if replace then
+				result.name = replace
+			end
+		end
+
+        if recipe.main_product then
+			local replace = replacements[recipe.main_product]
+		if replace then recipe.main_product = replace end
+        end
+    end
+end
+
 if mods["James-Oil-Processing"] and mods["metal-and-stars"] then
     local replacements = {
         ["VPE-bitumen"] = "bitumen",
@@ -108,6 +136,7 @@ if mods["James-Oil-Processing"] and mods["metal-and-stars"] then
         end
     end
 end
+
 if mods["bobplates"] and mods["space-age"] and mods["James-Oil-Processing"] then
     data.raw.item["carbon"].icon = "__space-age__/graphics/icons/carbon.png"
     data.raw.item["carbon"].icon_size = 64
