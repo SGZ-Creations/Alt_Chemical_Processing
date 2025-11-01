fdd73a12879597c359e80661fe962a371e0b5d5c
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-01 14:38:17 +0100

    Temp fix easy Wind

--- Alt_Chemical_Processing/data-final-fixes.lua
+++ Alt_Chemical_Processing/data-final-fixes.lua
@@ -1,6 +1,7 @@
 require("prototypes.Gregtorio.Recipes")
 require("prototypes.Gregtorio.Tech")
 
+require("prototypes.WindTurbines.Misc")
 require("mods.duplicate-items-final-fix")
 require("prototypes.bobs.recipes-final-fix")
 require("mods.filters")
