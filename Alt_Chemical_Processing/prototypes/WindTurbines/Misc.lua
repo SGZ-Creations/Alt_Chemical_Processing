1d409dd1bd1f0c9849d975c99bba515c0e09b0ed
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-01 15:01:21 +0100

    Locals

--- Alt_Chemical_Processing/prototypes/WindTurbines/Misc.lua
+++ Alt_Chemical_Processing/prototypes/WindTurbines/Misc.lua
@@ -5,33 +5,35 @@ Tech["easy-wind-turbine-3"].hidden = false
 Tech["easy-wind-turbine-3"].enabled = true
 ]]
 if mods["easy-wind-turbine"] then
-	data:extend({
-		{
-			type = "technology",
-			name = "EWT-3",
-			icon = "__easy-wind-turbine__/graphics/windturbine3.png",
-			icon_size = 128,
-			prerequisites = {"easy-wind-turbine-2"},
-			effects = {
-				{
-					type = "unlock-recipe",
-					recipe = "easy-wind-turbine-3"
+	--if SS[BrokenWindTech"].value then
+		data:extend({
+			{
+				type = "technology",
+				name = "EWT-3",
+				icon = "__easy-wind-turbine__/graphics/windturbine3.png",
+				icon_size = 128,
+				prerequisites = {"easy-wind-turbine-2"},
+				effects = {
+					{
+						type = "unlock-recipe",
+						recipe = "easy-wind-turbine-3"
+					},
+					{
+						type = "unlock-recipe",
+						recipe = "upgrade-wind-turbine-2"
+					}
 				},
-				{
-					type = "unlock-recipe",
-					recipe = "upgrade-wind-turbine-2"
+				unit = {
+					count = 200,
+					ingredients = {
+						{"automation-science-pack", 1},
+						{"logistic-science-pack", 1},
+						{"chemical-science-pack", 1},
+						{"utility-science-pack", 1},
+					},
+					time = 20
 				}
-			},
-			unit = {
-				count = 200,
-				ingredients = {
-					{"automation-science-pack", 1},
-					{"logistic-science-pack", 1},
-					{"chemical-science-pack", 1},
-					{"utility-science-pack", 1},
-				},
-				time = 20
 			}
-		}
-	})
+		})
+	--end
 end
