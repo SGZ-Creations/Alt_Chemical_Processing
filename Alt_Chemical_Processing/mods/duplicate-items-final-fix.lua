57865313fef547d7c60e497cb4558f368161e8fd
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-11-30 03:47:21 +0100

    Removes duplicate gear wheel

--- Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
+++ Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
@@ -30,6 +30,32 @@ if mods["bobplates"] and mods["space-age"] then
     end
 end
 
+if mods["bobplates"] and mods["linox"] then
+    local replacements = {
+        ["tungsten-gear-wheel"] = "bob-tungsten-gear-wheel",
+    }
+    for _, recipe in pairs(data.raw.recipe) do
+        for _, ingredient in pairs(recipe.ingredients or {}) do
+            local replace = replacements[ingredient.name]
+				if replace then
+                ingredient.name = replace
+            end
+        end
+
+		for _, result in pairs(recipe.results or {}) do
+			local replace = replacements[result.name]
+				if replace then
+				result.name = replace
+			end
+		end
+
+        if recipe.main_product then
+			local replace = replacements[recipe.main_product]
+		if replace then recipe.main_product = replace end
+        end
+    end
+end
+
 if mods["bobplates"] and mods["SaltedFish"] then
     local replacements = {
         ["bob-salt"] = "salt",
