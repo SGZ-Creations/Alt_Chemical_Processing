f25f436164636f62200fc0de7408e36128de652f
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-12-01 05:54:12 +0100

    less speed

--- Alt_Chemical_Processing/prototypes/MetalStars/FlyingRobots.lua
+++ Alt_Chemical_Processing/prototypes/MetalStars/FlyingRobots.lua
@@ -5,8 +5,8 @@ local ConstructBot = data.raw["construction-robot"]
 
 --Use Entity name when modifying bots with exception to stack size
 if mods["metal-and-stars"] then
-	LogicBot["antimatter-logistic-robot"].speed = 50
+	LogicBot["antimatter-logistic-robot"].speed = 5.0
 	LogicBot["antimatter-logistic-robot"].max_payload_size = 20
-	ConstructBot["antimatter-construction-robot"].speed = 50
+	ConstructBot["antimatter-construction-robot"].speed = 5.0
 	ConstructBot["antimatter-construction-robot"].max_payload_size = 20
 end
