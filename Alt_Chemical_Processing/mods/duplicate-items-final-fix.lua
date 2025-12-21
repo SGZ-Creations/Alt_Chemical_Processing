753cdfc659ee6ec590c0af190e654f4e0c5dc7a0
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2025-12-21 04:57:43 +0100

    Glass Merging

--- Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
+++ Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
@@ -112,7 +112,60 @@ if mods["bobplates"]and mods["bobelectronics"] and mods["metal-and-stars"] then
 end
 
 
-if mods["Nexus"]and mods["metal-and-stars"] then
+if mods["common-prototypes"]and mods["planetaris-arig"] and mods["maraxsis"] then
+    local replacements = {
+        ["planetaris-glass-panel"] = "glass-plate",
+        ["maraxsis-glass-panes"] = "glass-plate",
+    }
+    for _, recipe in pairs(data.raw.recipe) do
+        for _, ingredient in pairs(recipe.ingredients or {}) do
+            local replace = replacements[ingredient.name]
+				if replace then
+                ingredient.name = replace
+            end
+        end
+
+		for _, result in pairs(recipe.results or {}) do
+			local replace = replacements[result.name]
+				if replace then
+				result.name = replace
+			end
+		end
+
+        if recipe.main_product then
+			local replace = replacements[recipe.main_product]
+		if replace then recipe.main_product = replace end
+        end
+    end
+end
+
+if mods["Nexus"]and mods["planetaris-arig"] then
+    local replacements = {
+        ["tempered-glass"] = "planetaris-heavy-glass",
+    }
+    for _, recipe in pairs(data.raw.recipe) do
+        for _, ingredient in pairs(recipe.ingredients or {}) do
+            local replace = replacements[ingredient.name]
+				if replace then
+                ingredient.name = replace
+            end
+        end
+
+		for _, result in pairs(recipe.results or {}) do
+			local replace = replacements[result.name]
+				if replace then
+				result.name = replace
+			end
+		end
+
+        if recipe.main_product then
+			local replace = replacements[recipe.main_product]
+		if replace then recipe.main_product = replace end
+        end
+    end
+end
+
+if mods["Nexus"]and mods["common-prototypes"] then
     local replacements = {
         ["gold-wire"] = "gold-cable",
     }
