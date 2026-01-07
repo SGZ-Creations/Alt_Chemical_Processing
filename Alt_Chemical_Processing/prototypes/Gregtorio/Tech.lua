50ac79074ce5f1a2a369a039812da2b19b0972b7
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-07 04:30:11 +0100

    Removed Gregtorio changes.

--- Alt_Chemical_Processing/prototypes/Gregtorio/Tech.lua
+++
@@ -1,7 +0,0 @@
-local Tech = data.raw["technology"]
-
-if mods["Gregtorio"] then
-	table.insert(Tech["steel-processing"].effects, {type="unlock-recipe",recipe="steel-furnace"})
-	data.raw["furnace"]["steel-furnace"].crafting_speed = 4
-	table.insert(Tech["steam-macerator"].effects, {type="unlock-recipe",recipe="raw-tin-crushing"})
-end
