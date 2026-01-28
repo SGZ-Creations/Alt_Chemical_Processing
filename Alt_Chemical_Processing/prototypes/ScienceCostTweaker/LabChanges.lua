2fe40e36e581012b2b1a81a7372f54b9d931091a
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-28 15:02:03 +0100

    "s" Typo

--- Alt_Chemical_Processing/prototypes/ScienceCostTweaker/LabChanges.lua
+++ Alt_Chemical_Processing/prototypes/ScienceCostTweaker/LabChanges.lua
@@ -1,5 +1,5 @@
 Lab = data.raw.lab
 if mods["ScienceCostTweakerM"] and mods["bobtech"] then
-    table.insert(Lab["lab"].inputs, "logistics-science-pack")
-	table.insert(Lab["bob-lab-2"].inputs, "logistics-science-pack")
+    table.insert(Lab["lab"].inputs, "logistic-science-pack")
+	table.insert(Lab["bob-lab-2"].inputs, "logistic-science-pack")
 end
