2318298a50bab32e1aca1f1cfd051cb2e7d12447
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-01-29 12:54:10 +0100

    FlowControl with SeaBlock

--- Alt_Chemical_Processing/prototypes/Base/tech.lua
+++ Alt_Chemical_Processing/prototypes/Base/tech.lua
@@ -1,6 +1,14 @@
+---@class LuaSettings
 local SS = settings.startup
+---@class TechPrototype
 local Tech = data.raw["technology"]
 
 if SS["LiquidCoal"].value then
 	table.insert(Tech["coal-liquefaction"].effects, {type = "unlock-recipe", recipe = "acid-coal-liquefaction"})
+end
+
+if mods["Flow Control"] and mods["SeaBlock"] then
+	table.insert(Tech["sb-startup3"].effects, {type = "unlock-recipe", recipe = "pipe-straight"})
+	table.insert(Tech["sb-startup3"].effects, {type = "unlock-recipe", recipe = "pipe-junction"})
+	table.insert(Tech["sb-startup3"].effects, {type = "unlock-recipe", recipe = "pipe-elbow"})
 end
