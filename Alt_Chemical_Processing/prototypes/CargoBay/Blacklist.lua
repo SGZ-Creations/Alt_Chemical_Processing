f52f0d692de94ba0c6036e3a54ea1abd2e7a6773
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-02-28 08:11:43 +0100

    CargoBay (Rubia)

--- Alt_Chemical_Processing/prototypes/CargoBay/Blacklist.lua
+++ Alt_Chemical_Processing/prototypes/CargoBay/Blacklist.lua
@@ -2,7 +2,7 @@
 local SS = settings.startup
 
 if mods["cargo-bay-inserters"]and mods["rubia"] then
-	if SS["RubiaCargoBayCrash"].value == true then --Due to Rubia version 0.69.130 the mod will force crash your game unless you modify it by adding: "--" to the require code in control stage & data-update to force load your game Starting to be the same D*** as SE author.
+	if SS["RubiaCargoBayCrash"].value == true then --Due to Rubia version 0.69.130 the mod will force crash your game unless you modify it by adding: "--" to the require code in control stage & data-update to force load your game.
 		data.raw["mod-data"]["cargo-bay-inserters"].data.surface_name_blacklist["rubia"] = nil
 	end
 end
