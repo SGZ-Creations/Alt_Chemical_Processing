b0aa7652e7c5187894929db619b05a42e0719757
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-03-29 10:09:27 +0200

    Recipe?

--- Alt_Chemical_Processing/prototypes/3rdPartyFixes/WarpDriveMachine.lua
+++ Alt_Chemical_Processing/prototypes/3rdPartyFixes/WarpDriveMachine.lua
@@ -1,3 +1,3 @@
 if mods["Warp-Drive-Machine"] then
-	data.raw.item["warp_pipe_1"].icon = "__Warp-Drive-Machine__/graphics/technology/warp_pipe.png"
+	data.raw["recipe"]["warp_pipe_1"].icon = "__Warp-Drive-Machine__/graphics/technology/warp_pipe.png"
 end
