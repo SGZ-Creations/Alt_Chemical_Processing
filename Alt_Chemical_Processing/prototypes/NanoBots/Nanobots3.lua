8c38d2e482d7aa8c797594b83e6d7fb3dd4bcc76
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-06-07 21:18:30 +0200

    nano3 ajustments

--- Alt_Chemical_Processing/prototypes/NanoBots/Nanobots3.lua
+++ Alt_Chemical_Processing/prototypes/NanoBots/Nanobots3.lua
@@ -4,32 +4,27 @@ local Recipe = data.raw["recipe"]
 local Tech = data.raw["technology"]
 local SS = settings.startup
 
-
-if mods["Nanobots3"]and mods["SeaBlock"] then
-	Tech["nanobots"].research_trigger = {type ="craft-item", item ="bob-basic-circuit-board", amount = 20}
-	Tech["nanobots"].unit = nil
-	Tech["nanobots"].prerequisites = nil
-
-	Recipe["gun-nano-emitter"].ingredients = {
-		{type ="item", name="bob-basic-circuit-board", amount = 10},
-		{type ="item", name="copper-plate", amount = 5},
-		{type ="item", name="iron-plate", amount = 5},
-	}
-end
-
-if mods["Nanobots3"]and mods["pypostprocessing"] then
-	Tech["nanobots"].research_trigger = {type ="craft-item", item ="repair-pack", amount = 1}
-	Tech["nanobots"].unit = nil
-	Tech["nanobots"].prerequisites = nil
-
-	Recipe["gun-nano-emitter"].ingredients = {
-		{type ="item", name="repair-pack", amount = 5},
-		{type ="item", name="copper-plate", amount = 5},
-		{type ="item", name="iron-plate", amount = 5},
-	}
-end
-
 if mods["Nanobots3"]then
+	if mods["SeaBlock"] then
+		Tech["nanobots"].research_trigger = {type ="craft-item", item ="bob-basic-circuit-board", amount = 20}
+		Tech["nanobots"].unit = nil
+		Tech["nanobots"].prerequisites = nil
+		Recipe["gun-nano-emitter"].ingredients = {
+			{type ="item", name="bob-basic-circuit-board", amount = 10},
+			{type ="item", name="copper-plate", amount = 5},
+			{type ="item", name="iron-plate", amount = 5},
+		}
+	end
+	if mods["pypostprocessing"] then
+		Tech["nanobots"].research_trigger = {type ="craft-item", item ="repair-pack", amount = 1}
+		Tech["nanobots"].unit = nil
+		Tech["nanobots"].prerequisites = "repair-packs"
+		Recipe["gun-nano-emitter"].ingredients = {
+			{type ="item", name="repair-pack", amount = 5},
+			{type ="item", name="copper-plate", amount = 5},
+			{type ="item", name="iron-plate", amount = 5},
+		}
+	end
 	if SS["FreeNanobots"].value == true then
 		Recipe["ammo-nano-constructors"].ingredients = nil
 		Recipe["ammo-nano-termites"].ingredients = nil
