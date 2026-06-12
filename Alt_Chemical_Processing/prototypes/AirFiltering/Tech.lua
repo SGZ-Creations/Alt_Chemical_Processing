37fd02f327962e5ec9a07be8d72521d3dc3abe6e
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-06-12 12:43:07 +0200

    other fixes & changes.

--- Alt_Chemical_Processing/prototypes/AirFiltering/Tech.lua
+++ Alt_Chemical_Processing/prototypes/AirFiltering/Tech.lua
@@ -3,12 +3,12 @@ local SS = settings.startup
 
 if mods["k2-space-age"] then
 	if SS["FilterResearch"].value == "Costly" then
-
+		data.raw
 	end
 	if SS["FilterResearch"].value == "Normal" then
-	
+		data.raw
 	end
 	if SS["FilterResearch"].value == "Water" then
-	
+		data.raw
 	end
 end
