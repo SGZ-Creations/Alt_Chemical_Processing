948ed5e6171cbd578b5490b88719f089e2cfc65c
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-06-24 15:02:34 +0200

    depenency load order crash.

--- Alt_Chemical_Processing/prototypes/WaterWells/Recipes.lua
+++ Alt_Chemical_Processing/prototypes/WaterWells/Recipes.lua
@@ -3,9 +3,9 @@ local Recipes = data.raw.recipe
 
 if (mods["StoneWaterWell-ActuallyUpdated"] or mods["StoneWaterWell-NoRestrict"]) then
 	Recipes["stone-waterwell"].ingredients = {
-		{type ="item", name="stone", amount = 40},
-		{type ="item", name="stone-brick", amount = 8},
-		{type ="item", name="copper-plate", amount = 5},
+		{type ="item", name="stone", amount = 10},
+		{type ="item", name="stone-brick", amount = 10},
+		{type ="item", name="copper-plate", amount = 10},
 		{type ="item", name="offshore-pump", amount = 1},
 	}
 end
