e7394b753576988ac3e0957076af931ee4ef31c3
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-06-29 04:37:17 +0200

    muluna Aluminium fix

--- Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
+++ Alt_Chemical_Processing/mods/duplicate-items-final-fix.lua
@@ -106,6 +106,32 @@ if mods["bobplates"]and mods["bobelectronics"] and mods["metal-and-stars"] and n
 end
 
 
+if mods["bobplates"]and mods["planet-muluna"] and not mods["TIMSABA"] then
+    local replacements = {
+        ["aluminum-plate"] = "bob-aluminium-plate",
+    }
+    for _, recipe in pairs(data.raw.recipe) do
+        for _, ingredient in pairs(recipe.ingredients or {}) do
+            local replace = replacements[ingredient.name]
+				if replace then
+                ingredient.name = replace
+            end
+        end
+
+		for _, result in pairs(recipe.results or {}) do
+			local replace = replacements[result.name]
+				if replace then
+				result.name = replace
+			end
+		end
+
+        if recipe.main_product then
+			local replace = replacements[recipe.main_product]
+		if replace then recipe.main_product = replace end
+        end
+    end
+end
+
 if mods["common-prototypes"]and mods["planetaris-arig"] and mods["maraxsis"]and mods["foliax"] then
     local replacements = {
         ["planetaris-glass-panel"] = "glass-plate",
