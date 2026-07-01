c70bdbb530a64fb2a99df34225165e6c1193606c
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-07-01 11:56:33 +0200

    Missing prerequisites changes.

--- Alt_Chemical_Processing/prototypes/SignalTransmission/Tech.lua
+++ Alt_Chemical_Processing/prototypes/SignalTransmission/Tech.lua
@@ -1,5 +1,6 @@
 local Tech = data.raw["technology"]
 
 if mods["aai-signal-transmission"] and mods["pypostprocessing"] then
+	Tech["aai-signal-transmission"].prerequisites = {"automation-science-pack"}
 	Tech["aai-signal-transmission"].unit.ingredients = {{"automation-science-pack", 1}}
 end
