e107c7ed9f647b1d4b0462bc1222aee908019b14
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-07-07 20:11:26 +0200

    TransportDrones.

--- Alt_Chemical_Processing/prototypes/TransportDroens/FixRecipe.lua
+++ Alt_Chemical_Processing/prototypes/TransportDroens/FixRecipe.lua
@@ -3,18 +3,33 @@ local Item = data.raw["item"]
 ---@class data.RecipePrototype
 local Recipe = data.raw["recipe"]
 
-if mods["pypostprocessing"]then
-	Recipe["road"].category = "crafting-with-fluid"
-	Recipe["road"].ingredients = {
-		{type = "fluid", name = "tar", amount = 10},
-		{type = "item", name = "stone", amount = 5},
-		{type = "item", name = "gravel", amount = 10},
-		{type = "item", name = "soil", amount = 20},
-	}
-	if mods["vanilla-loaders-hd"]then
+
+if mods["Transport_Drones_Continued"]then
+	if mods["pypostprocessing"]then
+		Recipe["road"].category = "crafting-with-fluid"
+		Recipe["road"].ingredients = {
+			{type = "fluid", name = "tar", amount = 10},
+			{type = "item", name = "stone", amount = 5},
+			{type = "item", name = "gravel", amount = 10},
+			{type = "item", name = "soil", amount = 20},
+		}
 		Recipe["fast-road"].ingredients = {
 			{type = "item", name = "road", amount = 1},
 			{type = "fluid", name = "pitch", amount = 20},
+			{type = "item", name = "concrete", amount = 20},
+			{type = "item", name = "stone-brick", amount = 20},
+		}
+	end
+	if mods["bobplates"] and mods["bobrevamp"] then
+		--Recipe["road"].category = "crafting-with-fluid"
+		Recipe["road"].ingredients = {
+			{type = "item", name = "stone-brick", amount = 10},
+			{type = "item", name = "", amount = 10},
+			{type = "item", name = "", amount = 20},
+		}
+		Recipe["fast-road"].ingredients = {
+			{type = "item", name = "road", amount = 10},
+			{type = "fluid", name = "crude-oil", amount = 100},
 			{type = "item", name = "concrete", amount = 20},
 			{type = "item", name = "stone-brick", amount = 20},
 		}
