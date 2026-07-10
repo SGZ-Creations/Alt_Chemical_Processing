180dae93044d023269b6bd3dee280813573779e4
SGZ-Creations <59801736+SGZ-Creations@users.noreply.github.com>
2026-07-10 11:59:12 +0200

    Bob Transport drone chnages.

--- Alt_Chemical_Processing/prototypes/TransportDroens/FixRecipe.lua
+++ Alt_Chemical_Processing/prototypes/TransportDroens/FixRecipe.lua
@@ -21,17 +21,70 @@ if mods["Transport_Drones_Continued"]then
 		}
 	end
 	if mods["bobplates"] and mods["bobrevamp"] then
-		--Recipe["road"].category = "crafting-with-fluid"
 		Recipe["road"].ingredients = {
 			{type = "item", name = "stone-brick", amount = 10},
-			{type = "item", name = "stone", amount = 10},
-			{type = "item", name = "coal", amount = 20},
+			{type = "item", name = "coal", amount = 10},
 		}
 		Recipe["fast-road"].ingredients = {
 			{type = "item", name = "road", amount = 10},
 			{type = "fluid", name = "crude-oil", amount = 100},
-			{type = "item", name = "concrete", amount = 20},
-			{type = "item", name = "stone-brick", amount = 20},
+			{type = "item", name = "concrete", amount = 10},
+		}
+		Recipe["drone-dispatcher"].ingredients = {
+			{type = "item", name = "bob-invar-alloy", amount = 10},
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-glass", amount = 100},
+			{type = "item", name = "bob-solder", amount = 20},
+		}
+		Recipe["request-depot"].ingredients = {
+			{type = "item", name = "bob-bronze-alloy", amount = 25},
+			{type = "item", name = "electronic-circuit", amount = 20},
+			{type = "item", name = "bob-glass", amount = 100},
+			{type = "item", name = "bob-solder", amount = 20},
+		}
+		Recipe["supply-depot"].ingredients = {
+			{type = "item", name = "bob-bronze-alloy", amount = 25},
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-glass", amount = 100},
+			{type = "item", name = "bob-solder", amount = 20},
+		}
+		Recipe["active-depot"].ingredients = {
+			{type = "item", name = "bob-silver-plate", amount = 50},
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-insulated-cable", amount = 25},
+			{type = "item", name = "bob-glass", amount = 100},
+		}
+		Recipe["buffer-depot"].ingredients = {
+			{type = "item", name = "bob-silver-plate", amount = 50},
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-insulated-cable", amount = 25},
+			{type = "item", name = "bob-glass", amount = 100},
+		}
+		Recipe["storage-depot"].ingredients = {
+			{type = "item", name = "bob-silver-plate", amount = 50},
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-insulated-cable", amount = 25},
+			{type = "item", name = "bob-glass", amount = 100},
+		}
+		Recipe["fluid-depot"].ingredients = {
+			{type = "item", name = "bob-bronze-alloy", amount = 25},
+			{type = "item", name = "bob-steel-pipe", amount = 10},
+			{type = "item", name = "bob-lead-plate", amount = 50},
+			{type = "item", name = "copper-plate", amount = 50},
+		}
+		Recipe["fuel-depot"].ingredients = {
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "bob-silver-plate", amount = 50},
+			{type = "item", name = "bob-bronze-alloy", amount = 25},
+			{type = "item", name = "bob-glass", amount = 100},
+			{type = "item", name = "bob-rubber", amount = 20},
+		}
+		Recipe["transport-drones"].ingredients = {
+			{type = "item", name = "electronic-circuit", amount = 10},
+			{type = "item", name = "engine-unit", amount = 10},
+		}
+		Recipe["transport-drones"].result = {
+			{type = "item", name = "transport-drones", amount = 10},
 		}
 	end
 end
--- Alt_Chemical_Processing/prototypes/TransportDroens/FixTech.lua
+++ Alt_Chemical_Processing/prototypes/TransportDroens/FixTech.lua
@@ -1,5 +1,8 @@
 ---@class data.RecipePrototype
 local Tech = data.raw["technology"]
+---@class data.RecipePrototype
+local Recipe = data.raw["recipe"]
+
 
 if mods["pypostprocessing"]then
     if mods["Transport_Drones_Continued"] then
@@ -23,4 +26,25 @@ if mods["pypostprocessing"]then
 			{"py-science-pack-1",  1}
 		}
     end
+	if mods["bobplates"] and mods["bobrevamp"] and mods["bobtech"] then
+	Tech["fast-road"].research_trigger = {type ="craft-item", item ="road", amount = 1000}
+        Tech["fast-road"].unit = nil
+        Tech["transport-system"].unit.ingredients = {
+			{"automation-science-pack",  2},
+		}
+        Tech["transport-fluids"].unit.ingredients = {
+			{"automation-science-pack",  2},
+		}
+        Tech["transport-buffering"].unit.ingredients = {
+			{"automation-science-pack",  2},
+		}
+        Tech["transport-active-supply"].unit.ingredients = {
+			{"automation-science-pack",  2},
+		}
+        Tech["transport-multi-pipe"].unit.ingredients = {
+			{"automation-science-pack",  2},
+		}
+	end
+	if Recipe["active-depot"].hidden == true then Tech["transport-active-supply"].hidden = true end
+	if Recipe["active-depot"].hidden == true then Tech["transport-active-supply"].enabled = false end
 end
